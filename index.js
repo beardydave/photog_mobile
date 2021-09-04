108f6634acab296085ed7d6e1a263fc4b079c2c8
David Whittaker <davidwhittaker@Davids-MacBook-Pro.local>
2021-09-04 21:43:33 +0100

    menu added

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-function myFunction() {
-  var x = document.getElementById('myTopnav');
-  if (x.className === 'topnav') {
-    x.className += ' responsive';
-  } else {
-    x.className = 'topnav';
-  }
-}
+const nav = document.querySelector('nav');
+const menu = document.querySelector('.menu__icon');
+const navLinks = document.querySelectorAll('.nav__link');
+
+menu.addEventListener('click', () => menu.classList.toggle('menu__close'));
+
+menu.addEventListener('click', () => nav.classList.toggle('hide'));
+
+navLinks.forEach(link =>
+  link.addEventListener('click', () => {
+    nav.classList.add('hide');
+    menu.classList.remove('menu__close');
+  })
+);
